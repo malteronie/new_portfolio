fe9eea101b0c00f67e6f9a5ed9f66dbd1b2738da
malteronie <153739321+malteronie@users.noreply.github.com>
2025-04-03 14:45:38 +0200

    maj

---
+++ test.js
@@ -0,0 +1,22 @@
+const htmlP = document.getElementById("pop");
+const txt = htmlP.dataset.label;
+let i 	= 0 ;
+let u = 0;
+
+function showLetters()
+{
+    let timeOut ;
+    if(i < txt.length)
+    {
+        htmlP.innerHTML += `<span>${txt[i]}</span>` ;
+        timeOut = setTimeout(showLetters,100)
+        i++
+    }
+    else
+    {
+        clearTimeout(timeOut);
+        console.log("end")
+        isOk=true
+    }
+}     
+showLetters()
